9bc50820c6ccd370608162f99e42c27542378633
Naren Dasan <naren@narendasan.com>
2018-12-01 15:18:38 -0800

    gpu stuff

--- src/rtop/datastreams/gpumonitor.rs
+++ src/rtop/datastreams/gpumonitor.rs
@@ -2,16 +2,47 @@ extern crate nvml_wrapper as nvml;
 
 use std::collections::HashMap;
 use nvml::{NVML, Device};
-use nvml::enum_wrappers::device::TemperatureSensor;
+use nvml::enum_wrappers::device::{TemperatureSensor, Clock, ClockId};
 
 use rtop::error::Error;
 use rtop::datastreams::datastream::GPUDataStream;
 
+#[derive(Debug)]
 pub struct GPUClks {
     pub Graphics: u32,
     pub SM: u32, 
     pub Mem: u32, 
     pub VideoEncDec: u32, 
+}
+
+impl GPUClks {
+    fn new(gpu: Option<&Device>) -> Result<Self, Error> {
+        match gpu {
+            Some(gpu) => Ok(Self {
+                Graphics: gpu.clock(Clock::Graphics, ClockId::Current)?,
+                SM: gpu.clock(Clock::SM, ClockId::Current)?,
+                Mem: gpu.clock(Clock::Memory, ClockId::Current)?,
+                VideoEncDec: gpu.clock(Clock::Video, ClockId::Current)?,
+            }),
+            None => Ok(Self {
+                Graphics: 0, 
+                SM: 0,
+                Mem: 0, 
+                VideoEncDec: 0,
+            })      
+        }
+    }
+}
+
+impl Clone for GPUClks {
+    fn clone(&self) -> Self {
+        Self {
+            Graphics: self.Graphics, 
+            SM: self.SM,
+            Mem: self.Mem,
+            VideoEncDec: self.VideoEncDec,
+        }
+    }
 }
 
 pub struct GPUMonitor {
@@ -25,7 +56,7 @@ pub struct GPUMonitor {
     pub clks: HashMap<u32, GPUClks>,
     pub clk_history: HashMap<u32, Vec<GPUClks>>, 
     pub driver_version: String, 
-    pub power_usage: HashMap<u32, f32>,
+    pub power_usage: HashMap<u32, u32>,
     pub power_usage_history: HashMap<u32, Vec<u32>>,
     pub processes: HashMap<u32, (u32, u64)>, //Device ID, (PID, Mem usage) 
     max_history_len: usize,
@@ -34,16 +65,16 @@ pub struct GPUMonitor {
 impl GPUDataStream for GPUMonitor {
     fn new(max_hist_len: usize) -> Self {        
         Self {
+            driver_version: "UNKNOWN".to_string(),
+            total_memory: HashMap::new(),
+            nvml_indices: HashMap::new(),
             names: HashMap::new(),
             temps: HashMap::new(),
             temp_history: HashMap::new(),
             memory_usage: HashMap::new(),
             memory_usage_history: HashMap::new(),
-            total_memory: HashMap::new(),
-            nvml_indices: HashMap::new(),
             clks: HashMap::new(),
             clk_history: HashMap::new(),  
-            driver_version: "UNKNOWN".to_string(),
             power_usage: HashMap::new(),
             power_usage_history: HashMap::new(),
             processes: HashMap::new(),
@@ -65,10 +96,6 @@ impl GPUDataStream for GPUMonitor {
             self.total_memory.insert(*id, gpu.memory_info()?.total);
             self.nvml_indices.insert(*id, gpu.index()?);
             self.names.insert(*id, gpu.name()?);
-            
-            //Update 1
-            self.memory_usage.insert(*id, gpu.memory_info()?.used);
-            self.memory_usage_history.insert(*id, vec![0; self.max_history_len]);
         } 
         
         Ok(())
@@ -90,12 +117,37 @@ impl GPUDataStream for GPUMonitor {
             }
             temp_history.push(gpu_temp);
 
-            self.memory_usage.insert(*id, gpu.memory_info()?.used); 
-        }
+            let mem_usage = gpu.memory_info()?.used;
+            self.memory_usage.insert(*id, mem_usage);
+            let mem_usage_history = self.memory_usage_history.entry(*id).or_insert(vec![0; self.max_history_len]);
+            while mem_usage_history.len() >= self.max_history_len {
+                mem_usage_history.remove(0);
+            }
+            mem_usage_history.push(mem_usage);
+ 
+            let clk = GPUClks::new(Some(gpu))?; 
+            self.clks.insert(*id, clk.clone());
+            let clks_history = self.clk_history.entry(*id).or_insert(vec![GPUClks::new(None)?; self.max_history_len]);
+            while clks_history.len() >= self.max_history_len {
+                clks_history.remove(0);
+            }
+            clks_history.push(clk.clone());
+                
+            //let pow = gpu.power_usage()?; 
+            //self.power_usage.insert(*id, pow);
+            //let power_history = self.power_usage_history.entry(*id).or_insert(vec![0; self.max_history_len]);
+            //while power_history.len() >= self.max_history_len {
+            //    power_history.remove(0);
+            //}
+            //power_history.push(pow);
 
+            //let processes = gpu.running_compute_processes()?;
+            //println!("{:?}", processes);
         
 
-        println!("{:?}", self.temp_history);
+        }
+
+        println!("{:?}", self.power_usage_history);
 
         Ok(())      
     }
