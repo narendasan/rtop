4c068d4dcbbc781f1fff01cbb20df04906d2dab9
Naren Dasan <naren@narendasan.com>
2019-04-21 21:45:29 -0700

    build.rs script

--- build.rs
+++ build.rs
@@ -1,13 +1,43 @@
+extern crate dirs;
 use std::env;
 use std::path::Path;
 use std::os::unix::fs;
+use std::process::{Command, Stdio};
+use std::str;
 
+//Apologies if you're trying to figure this out, ping me (@narendasan) if you need help :)
 fn main() -> std::io::Result<()> {
-    let gpu_monitor = env::var_os("CARGO_FEATURE_GPU-MONITOR").is_some();
+    let gpu_monitor = env::var_os("CARGO_FEATURE_GPU_MONITOR").is_some();
     if gpu_monitor {
-        if !Path::new("/usr/lib/libnvidia-ml.so").exists() {
-            fs::symlink("/usr/lib/nvidia-418/libnvidia-ml.so", "/usr/lib/libnvidia-ml.so")?;
-            return Ok(())
+        let ldconfig = Command::new("ldconfig").arg("-p")
+                                               .output()
+                                               .expect("Failed to run ldconfig");
+        let ldc_output = String::from_utf8_lossy(&(ldconfig.stdout[..])); 
+        let nvml_installed = ldc_output.split("\n").collect::<Vec<&str>>()
+                                      .iter()
+                                      .map(|x| x.split(" ").collect::<Vec<&str>>()[0])
+                                      .map(|x| x.replace("\t", ""))
+                                      .fold(false, |acc, lib| lib == "libnvidia-ml.so" || acc);
+        if !nvml_installed {
+            let nvidia_driver_version = match Command::new("cat").arg("/proc/driver/nvidia/version").output() {
+                Ok(out) => {
+                    let nvidia_driver_version_info = match str::from_utf8(&out.stdout) {
+                        Ok(out) => out.to_string(),
+                        Err(_) => return Err(std::io::Error::new(std::io::ErrorKind::Other, "Could not parse driver version"))
+                    };
+                    let first_line = nvidia_driver_version_info.split(".").map(|x| x.to_string()).collect::<Vec<String>>()[0].clone();
+                    match first_line.split("  ").map(|x| x.to_string()).collect::<Vec<String>>().last().cloned() {
+                        Some(ver) => ver,
+                        None => return Err(std::io::Error::new(std::io::ErrorKind::Other, "Could not parse driver version"))
+                    }
+                }
+                Err(_) => return Err(std::io::Error::new(std::io::ErrorKind::Other, "Could not find NVIDIA Driver verion"))
+            };
+            let home_dir = match dirs::home_dir() {
+                Some(p) => format!("{}", p.display()),
+                None => return Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to find path to home directory"))
+            };
+            fs::symlink("/usr/lib/nvidia-418/libnvidia-ml.so", &format!("{}/.local/lib/libnvidia-ml.so", home_dir))?;
         }
     }
     return Ok(())
